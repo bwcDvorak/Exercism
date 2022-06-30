59a5b3878003a21f8a24d460ebbe4e8a7688ef81
Brian Covey <bcovey@Brians-MacBook-Pro.local>
2022-06-30 12:08:40 -0700

    ACTUAL gigasecond solution

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,1 +1,19 @@
+import Foundation
+
 //Solution goes in Sources
+
+struct Gigasecond {
+    var description: String
+
+    init? (from: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+//        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+
+        var futureTime = dateFormatter.date(from: from)
+        futureTime = futureTime?.addingTimeInterval(1000000000)
+        
+        description = dateFormatter.string(from: futureTime ?? Date())
+    }
+}
