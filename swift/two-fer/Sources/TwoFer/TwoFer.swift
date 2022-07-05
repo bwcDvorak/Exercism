c76894c8590e7e7b5c27eb7b175b5ef6310e7375
Brian Covey <bcovey@Brians-MacBook-Pro.local>
2022-07-04 18:36:20 -0700

    twoFer solution

---
+++ swift/two-fer/Sources/TwoFer/TwoFer.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+//Solution goes in Sources
+struct TwoFer {
+    init (_ name:String) {
+    }
+    
+    static func twoFer( name: String = "you") -> String {
+        return "One for \(name), one for me."
+    }
+}
