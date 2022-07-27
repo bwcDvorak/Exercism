606b7bb716f09815820ae6b1c973ad9becc0b054
Brian Covey <bcovey@Brians-MacBook-Pro.local>
2022-07-27 15:17:16 -0700

    diamond solution

--- swift/diamond/Sources/Diamond/Diamond.swift
+++ swift/diamond/Sources/Diamond/Diamond.swift
@@ -1,1 +1,25 @@
+import Foundation
+
 //Solution goes in Sources
+
+struct Diamond {
+
+    static func makeDiamond(letter: Character) -> [String] {
+        let myAlphabet = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        var myDiamond = [String]()
+
+        guard let myIndex = myAlphabet.firstIndex(of: letter) else {
+            return [""]
+        }
+
+        for i in 0...myIndex {
+            var myLine = Array(repeating: " ", count: myIndex*2+1)
+                myLine[myIndex-i] = String(myAlphabet[i])
+                myLine[myIndex+i] = String(myAlphabet[i])
+                myDiamond.append(String(describing: myLine.joined()))
+            }
+        
+            myDiamond += myDiamond.dropLast().reversed()
+            return myDiamond
+    }
+}
