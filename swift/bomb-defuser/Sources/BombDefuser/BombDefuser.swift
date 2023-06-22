69cd4fda8e6138dd279d3dd9fbec70f3902e8cba
Brian Covey <dvorak@mac.com>
2023-06-22 05:59:42 -0700

    adding improved solution

--- swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
+++ swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
@@ -10,21 +10,19 @@ let rotate = { (tuple: (a: String, b: String, c: String)) -> (String, String, St
     return (b, c, a)
 }
 
-func makeShuffle(
-  flipper: @escaping ((String, String, String)) -> (String, String, String),
-  rotator: @escaping ((String, String, String)) -> (String, String, String)
-) -> (UInt8, (String, String, String)) -> (String, String, String) {
-    
-    return { (myID: UInt8, myWires: (String, String, String)) -> (String, String, String) in
-        var myWires = myWires
-        var myID = myID
-        var counter = 0
+func makeShuffle(flipper: @escaping ((String, String, String)) -> (String, String, String),
+                 rotator: @escaping ((String, String, String)) -> (String, String, String))
+    -> (UInt8, (String, String, String)) -> (String, String, String)
+{
+    return { myID, myWires in
+        var currentWires = myWires
+        var currentID = myID
         
-        while counter <= 7 {
-        myWires = myID % 2 == 0 ? flipper(myWires) : rotator(myWires)
-        myID = myID / 2
-        counter += 1
+        for _ in 0...7 {
+            currentWires = currentID % 2 == 0 ? flipper(currentWires) : rotator(currentWires)
+            currentID = currentID / 2
         }
-        return myWires
+        
+        return currentWires
     }
 }
