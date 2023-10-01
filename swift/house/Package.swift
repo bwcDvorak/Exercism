56c75f43a47584f3d9d08e0d8347979120df54b5
Brian Covey <dvorak@mac.com>
2023-10-01 05:47:54 -0700

    update Scale Generator for lesson revision on Exercism

---
+++ swift/house/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "House",
+  products: [
+    .library(
+      name: "House",
+      targets: ["House"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "House",
+      dependencies: []),
+    .testTarget(
+      name: "HouseTests",
+      dependencies: ["House"]),
+  ]
+)
